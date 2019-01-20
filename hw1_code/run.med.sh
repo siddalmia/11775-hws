c53c97b69cffee1dc6c91bae815c87c49f0d0823
Siddharth Dalmia <sdalmia@andrew.cmu.edu>
2019-01-20 01:08:12 -0500

    Minor Changes

--- hw1_code/run.med.sh
+++ hw1_code/run.med.sh
@@ -23,9 +23,9 @@ for event in P001 P002 P003; do
   python scripts/train_svm.py $event "kmeans/" $feat_dim_mfcc mfcc_pred/svm.$event.model || exit 1;
   # apply the svm model to *ALL* the testing videos;
   # output the score of each testing video to a file ${event}_pred 
-  python scripts/test_svm.py mfcc_pred/svm.$event.model "kmeans/" $feat_dim_mfcc mfcc_pred/${event}_pred || exit 1;
+  python scripts/test_svm.py mfcc_pred/svm.$event.model "kmeans/" $feat_dim_mfcc mfcc_pred/${event}_mfcc.lst || exit 1;
   # compute the average precision by calling the mAP package
-  ap list/${event}_val_label mfcc_pred/${event}_pred
+  ap list/${event}_val_label mfcc_pred/${event}_mfcc.lst
 done
 
 echo ""
@@ -41,8 +41,8 @@ for event in P001 P002 P003; do
   python scripts/train_svm.py $event "asrfeat/" $feat_dim_asr asr_pred/svm.$event.model || exit 1;
   # apply the svm model to *ALL* the testing videos;
   # output the score of each testing video to a file ${event}_pred 
-  python scripts/test_svm.py asr_pred/svm.$event.model "asrfeat/" $feat_dim_asr asr_pred/${event}_pred || exit 1;
+  python scripts/test_svm.py asr_pred/svm.$event.model "asrfeat/" $feat_dim_asr asr_pred/${event}_asr.lst || exit 1;
   # compute the average precision by calling the mAP package
-  ap list/${event}_val_label asr_pred/${event}_pred
+  ap list/${event}_val_label asr_pred/${event}_asr.lst
 done
 
